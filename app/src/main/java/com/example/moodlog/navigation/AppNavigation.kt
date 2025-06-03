9b3b0f0d716efda72dc45101cfa87c608380508c
Souluigi <marcos2003mlr@gmail.com>
2025-06-03 09:26:36 -0300

    Estilização da barra de navegação

--- app/src/main/java/com/example/moodlog/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/moodlog/navigation/AppNavigation.kt
@@ -1,28 +1,27 @@
 package com.example.moodlog.navigation
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults
-import androidx.compose.material3.CheckboxDefaults.colors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.moodlog.R
+import com.example.moodlog.ui.theme.Blue_My
+import com.example.moodlog.ui.theme.White_My
 import com.example.moodlog.ui.theme.Yellow_My
 import com.example.moodlog.view.MoodHistory
 import com.example.moodlog.view.MoodScreen
@@ -37,43 +36,81 @@ fun AppNavigation() {
     Scaffold(
         bottomBar = {
             BottomAppBar(
-               containerColor = Yellow_My
+                modifier = Modifier.height(65.dp),
+                containerColor = Yellow_My
             ) {
                 Row(
-                ) {  IconButton(
-                    modifier = Modifier.weight(1f),
-                    onClick = { navController.navigate(Routes.MOOD_SCREEN) },
-                    enabled = currentRoute != Routes.MOOD_SCREEN
                 ) {
-                    Icon(
-                        modifier = Modifier.size(50.dp),
-                        imageVector = Icons.Default.Home,
-                        contentDescription = "Tela de humor"
-                    )
-                }
                     IconButton(
-                        modifier = Modifier.weight(1f),
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(56.dp),
+                        onClick = { navController.navigate(Routes.MOOD_SCREEN) },
+                        enabled = currentRoute != Routes.MOOD_SCREEN
+                    ) {
+                        val isSelected = currentRoute == Routes.MOOD_SCREEN
+                        val iconColor = if (isSelected) Blue_My else White_My
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.icon_house),
+                                contentDescription = "Tela de Principal",
+                                tint = iconColor
+                            )
+                            Text(
+                                text = "Início",
+                                fontSize = 12.sp,
+                                modifier = Modifier.padding(top = 0.dp),
+                                color = iconColor
+                            )
+                        }
+                    }
+                    IconButton(
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(56.dp),
                         onClick = { navController.navigate(Routes.MOOD_HISTORY) },
                         enabled = currentRoute != Routes.MOOD_HISTORY
                     ) {
-                        Icon(
-                            modifier = Modifier.size(50.dp),
-                            imageVector = Icons.Default.List,
-                            contentDescription = "Histórico de humor"
-                        )
+                        val isSelected = currentRoute == Routes.MOOD_HISTORY
+                        val iconColor = if (isSelected) Blue_My else White_My
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.icon_bookmark),
+                                contentDescription = "Tela de histórico",
+                                tint = iconColor
+                            )
+                            Text(
+                                text = "Histórico",
+                                fontSize = 12.sp,
+                                modifier = Modifier.padding(top = 0.dp),
+                                color = iconColor
+                            )
+                        }
                     }
                     IconButton(
-                        modifier = Modifier.weight(1f),
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(56.dp),
                         onClick = { navController.navigate(Routes.MOOD_STATISTIC) },
                         enabled = currentRoute != Routes.MOOD_STATISTIC
                     ) {
-                        Icon(
-                            modifier = Modifier.size(50.dp),
-                            imageVector = Icons.Default.AccountCircle,
-                            contentDescription = "Estatística de humor"
-                        )
-                    } }
-
+                        val isSelected = currentRoute == Routes.MOOD_STATISTIC
+                        val iconColor = if (isSelected) Blue_My else White_My
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.icon_chart),
+                                contentDescription = "Estatística de humor",
+                                tint = iconColor
+                            )
+                            Text(
+                                text = "Estátistica",
+                                fontSize = 12.sp,
+                                modifier = Modifier.padding(top = 0.dp),
+                                color = iconColor
+                            )
+                        }
+                    }
+                }
             }
         },
     ) { innerPadding ->
